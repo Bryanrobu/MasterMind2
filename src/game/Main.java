23a7fe95e3a42b545b9a63c94ae8e96ea2407224
Bryanrobu <1211898@student.roc-nijmegen.nl>
2024-12-10 11:12:29 +0100

    .

--- src/game/Main.java
+++ src/game/Main.java
@@ -4,10 +4,12 @@ import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
     private static String string;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         Scanner sc = new Scanner(System.in);
 
@@ -29,16 +31,23 @@ public class Main {
         int input = 0;
         boolean goedeinput = false;
 
-        while (!goedeinput) {
-            try {
+        while (!goedeinput)
+        {
+            try
+            {
                 input = sc.nextInt();
-                if (input > 0) {
+                if (input > 0)
+                {
                     goedeinput = true;
-                } else {
+                }
+                else
+                {
                     System.out.println("Oeps je kan geen negatief getal invullen,");
                     System.out.print("probeer het opnieuw: ");
                 }
-            } catch (InputMismatchException e) {
+            }
+            catch (InputMismatchException e)
+            {
                 System.out.println("Oeps, u heeft iets verkeerds ingevult");
                 System.out.print("Vul aub een nummer in voor de lengte van de code: ");
                 sc.next();
@@ -49,42 +58,48 @@ public class Main {
         System.out.print("Top! de code is nu " + input + " kleuren lang.");
 
         String[] geheimecodes = new String[input];
-        for (int lengte = 0; lengte < geheimecodes.length; lengte++) {
+        for (int lengte = 0; lengte < geheimecodes.length; lengte++)
+        {
             geheimecodes[lengte] = kleuren[rnd.nextInt(kleuren.length)];
         }
 
         // Loop 10 keer
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
 
             ronde++;
 
             // Vraag voor user input
             System.out.println("\n\nPoging: " + ronde);
-//            System.out.print("Voer uw gok in: ");
-//
-//
+
             String[] pogingen = new String[geheimecodes.length];
-//            for (int lengte = 0; lengte < geheimecodes.length; lengte++) {
-//                pogingen[lengte] = sc.next();
-//            }
 
-
-            for (int y = 0; y < geheimecodes.length; y++) {
+            for (int y = 0; y < geheimecodes.length; y++)
+            {
                 boolean matchFound = false;
-                y++; System.out.print("Kleur " + y + ": "); y--;
+                y++;
+                System.out.print("Kleur " + y + ": ");
+                y--;
                 pogingen[y] = sc.next().toLowerCase();
-                for (String str : kleuren) {
-                    if (pogingen[y].equals(str)) {
+                for (String str : kleuren)
+                {
+                    if (pogingen[y].equals(str))
+                    {
                         matchFound = true;
-                        break;
+                        break; //hoe staat Robert tov de break, ik vind dat je dan een while moet gebruiken dan heb je die niet nodig
                     }
                 }
-                while (!matchFound) {
+                while (!matchFound)
+                {
                     System.out.println("Vul een van de bovenstaande kleuren in aub");
-                    y++; System.out.print("Kleur " + y + ": "); y--;
+                    y++;
+                    System.out.print("Kleur " + y + ": ");
+                    y--;
                     pogingen[y] = sc.next();
-                    for (String str : kleuren) {
-                        if (pogingen[y].equals(str)) {
+                    for (String str : kleuren)
+                    {
+                        if (pogingen[y].equals(str))
+                        {
                             matchFound = true;
                             break;
                         }
@@ -93,22 +108,28 @@ public class Main {
             }
 
 
-
             // Controleer eerste poging
-            for (int o = 0; o < geheimecodes.length; o++) {
+            for (int o = 0; o < geheimecodes.length; o++)
+            {
 
                 String resultaat = "- "; //standaarwaarde word elke poging terug gezet naar fout
-                if (pogingen[o].equals(geheimecodes[o])) {
+                if (pogingen[o].equals(geheimecodes[o]))
+                {
                     resultaat = "Z "; //bij een 1 to 1 match goed
                     score++;
-                } else {
-                    for (int j = 0; j < geheimecodes.length; j++) {
+                }
+                else
+                {
+                    for (int j = 0; j < geheimecodes.length; j++)
+                    {
 
-                        if (pogingen[j].equals(geheimecodes[j])) {
+                        if (pogingen[j].equals(geheimecodes[j]))
+                        {
                             continue; //als hij 1 en 1 wilt verglijken skipt hij dit en gaat naar 1 en 2 verglijken
                         }
 
-                        if (pogingen[o].equals(geheimecodes[j])) {
+                        if (pogingen[o].equals(geheimecodes[j]))
+                        {
                             resultaat = "W "; //als een van de andere kleuren overeenkomt met je gok wordt de aanwezigheid aangegeven met een W
                             break;
                         }
@@ -117,19 +138,26 @@ public class Main {
                 System.out.print(resultaat);
             }
 
-            if (score == geheimecodes.length) {
+            if (score == geheimecodes.length)
+            {
                 i = 11;
                 gameGewonnen = true;
-            } else {
+            }
+            else
+            {
                 score = 0;
             }
         }
         System.out.println();
-        if (gameGewonnen) {
+        if (gameGewonnen)
+        {
             System.out.println("\nGefeliciteerd je hebt gewonnen!!!");
-        } else {
+        }
+        else
+        {
             System.out.print("\nHelaas, de code was: ");
-            for (String geheimecode : geheimecodes) {
+            for (String geheimecode : geheimecodes)
+            {
                 System.out.print(geheimecode + " ");
 
             }
